2ac2f5ff99d18dc3a959268851e732ac51960a32
abdullah0707 <abdullahelgammal25@gmail.com>
2024-08-24 18:54:54 +0300

    up 49

--- pages/index.vue
+++ pages/index.vue
@@ -126,4 +126,35 @@ definePageMeta({
     border-color: transparent;
   }
 }
+
+.animate-ping {
+  animation: ping 3s cubic-bezier(0, 0, 0.2, 1) infinite;
+}
+
+@keyframes ping {
+
+  75%,
+  100% {
+    transform: scale(2);
+    opacity: 0;
+  }
+}
+
+.animate-bounce {
+  animation: bounce 1.5s infinite;
+}
+
+@keyframes bounce {
+
+  0%,
+  100% {
+    transform: translateY(-25%);
+    animation-timing-function: cubic-bezier(0.8, 0, 1, 1);
+  }
+
+  50% {
+    transform: translateY(0);
+    animation-timing-function: cubic-bezier(0, 0, 0.2, 1);
+  }
+}
 </style>
